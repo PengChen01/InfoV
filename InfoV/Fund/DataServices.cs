2c500a3fce9e050724aa0452a144c910c06017d8
陈鹏 <1596058469@qq.com>
2022-04-04 23:03:07 +0800

    新建
    基于WPF的数据可视化，完全自开发

---
+++ InfoV/Fund/DataServices.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+
+namespace InfoV.Fund
+{
+  public enum DateRange
+  {
+    [Description("今年")]
+    ThisYear,
+    [Description("近3月")]
+    Near3Month,
+    [Description("近6月")]
+    Near6Month,
+    [Description("近1年")]
+    Near1Year,
+    [Description("近3年")]
+    Near3Year
+  }
+  public class Fund
+  {
+    public string Code { get; set; }
+    
+    public string Name { get; set; }
+    
+    public string Type { get; set; }
+
+    public string StartRecordTimeSpan { get; set; }
+    public string LastRecordTimeSpan { get; set; }
+    public DateRange DateRange { get; set; }
+
+    public IEnumerable<Point> Detail { get; set; }
+
+    public override string ToString()
+    {
+      return $"{Code}   {Name}";
+    }
+  }
+
+  public class DataServices
+  {
+    public static IEnumerable<Fund> GetFund(string findstring = null)
+    {
+      string resultstr = Util.HttpCommon.HttpGet(@"http://fund.eastmoney.com/js/fundcode_search.js?v=" + DateTime.Now.ToString("yyyyMMddHHmmss"));
+      //Regex regex = new Regex("var r = (?<Data>\\[\\[\"(?<Code>[0-9]{6})\",\".*?\",\"(?<Name>.*?)\",\"(?<Type>.*?)\".*?\\],{0,1})*\\]");
+      Regex regex = new Regex("var r = (?<Data>\\[(\\[\"(?<Code>[0-9]{6})\"\\,\".*?\"\\,\"(?<Name>.*?)\"\\,\"(?<Type>.*?)\".*?\\][\\,]{0,1})+\\])");
+      Match match = regex.Match(resultstr);
+      //int matchSize = match.Groups["Code"].Captures.Count;
+      
+      IEnumerable<Fund> s = match.Groups["Code"].Captures.Cast<Capture>().Select((v, i) => new { value = v.Value, index = i }).Join(match.Groups["Name"].Captures.Cast<Capture>().Select((v, i) => new { value = v.Value, index = i }), a => a.index, b => b.index, (a, b) => new { Code = a.value, Name = b.value, a.index }).Join(match.Groups["Type"].Captures.Cast<Capture>().Select((v, i) => new { value = v.Value, index = i }), a => a.index, b => b.index, (a, b) => new Fund { Code = a.Code, Name = a.Name, Type = b.value }).Where(w=> (findstring is null || (w.Code.Contains(findstring) || w.Name.Contains(findstring) || w.Type.Contains(findstring)))&&!w.Name.EndsWith("(后端)"));
+      return s;
+    }
+
+    public static void GetFundDetail(Fund fundCode)
+    {
+      if (fundCode.Detail == null)
+      {
+        if (fundCode.Type=="货币型"|| fundCode.Type == "QDII")
+        {
+          return;
+        }
+        string resultstr = Util.HttpCommon.HttpGet($"https://fund.eastmoney.com/pingzhongdata/{fundCode.Code}.js?v={DateTime.Now.ToString("yyyyMMddHHmmss")}");
+
+        if (resultstr.StartsWith("远程服务器返回错误"))
+        {
+          return;
+        }
+
+        Regex regex = new Regex("var Data_netWorthTrend = (?<Data>\\[(\\{\"x\"\\:(?<Date>[0-9]{13})\\,\"y\"\\:(?<Value>.*?)\\,.*?\\}[\\,]{0,1})+\\])");
+        Match match = regex.Match(resultstr);
+        fundCode.StartRecordTimeSpan = match.Groups["Date"].Captures.Cast<Capture>().First().Value;
+        fundCode.LastRecordTimeSpan = match.Groups["Date"].Captures.Cast<Capture>().Last().Value;
+        //fundCode.Detail = 
+        fundCode.Detail = match.Groups["Date"].Captures.Cast<Capture>().Select((v,i) =>new { index =i,value = Convert.ToSingle(v.Value) / 100000 } ).Join(match.Groups["Value"].Captures.Cast<Capture>().Select((v, i) => new { index = i, value = Convert.ToDouble(v.Value) }),a=>a.index,b=>b.index,(a,b)=>new Point{ X=a.value ,Y = b.value});
+      }
+    }
+  }
+}
---
+++ InfoV/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using InfoV.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InfoV
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+
+      
+      dosomething();
+    }
+
+    private void dosomething()
+    {
+      IEnumerable<Fund.Fund> funds = Fund.DataServices.GetFund();
+      CreateDashBoard.ProgramedLayout(this,funds);
+    }
+
+
+
+    
+
+    private void SearchFund_KeyUp(object sender, KeyEventArgs e)
+    {
+      if (e.Key== Key.Up||e.Key==Key.Down)
+      {
+        return;
+      }
+      CollectionView itemsViewOriginal = (CollectionView)CollectionViewSource.GetDefaultView(SearchFund.ItemsSource);
+      itemsViewOriginal.Filter = o =>
+      {
+        if (String.IsNullOrEmpty(SearchFund.Text))
+        {
+          return true;
+        }
+        else
+        {
+          if (o is Fund.Fund fund)
+          {
+            if (fund.Code.Contains(SearchFund.Text) || fund.Name.Contains(SearchFund.Text) || fund.Type.Contains(SearchFund.Text))
+            {
+              SearchFund.IsDropDownOpen = true;
+              return true;
+            }
+            else
+            {
+              return false;
+            }
+          }
+        }
+        return false;
+      };
+      //itemsViewOriginal.Refresh();
+
+      
+      // if datasource is a DataView, then apply RowFilter as below and replace above logic with below one
+      /* 
+       DataView view = (DataView) Cmb.ItemsSource; 
+       view.RowFilter = ("Name like '*" + Cmb.Text + "*'"); 
+      */
+    }
+
+    private void SearchFund_Selected(object sender, RoutedEventArgs e)
+    {
+      
+    }
+
+    private void SearchFund_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+      if (SearchFund.SelectedItem is Fund.Fund fund)
+      {
+        Fund.DataServices.GetFundDetail(fund);
+        if (fund.Detail!=null)
+        {
+          CreateDashBoard.RefreshDetail(this, fund);
+        }
+        
+      }
+    }
+  }
+}
